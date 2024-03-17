14be8120cc5b764d2381ea1c3af59c8ce4b501c2
Your Name <youremail@yourdomain.com>
2024-03-17 20:03:18 +0100

    Add index by searchable elements:
    manufacturer and name

--- docker/init.sql
+++ docker/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.bikes (
     id UUID NOT NULL,
-    description VARCHAR(255) NULL,
+    description VARCHAR(500) NULL,
     manufacturer VARCHAR(255) NULL,
     name VARCHAR(255) NOT NULL,
     price FLOAT8 NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS public.bikes (
 
 CREATE TABLE IF NOT EXISTS public.items (
 	id uuid NOT NULL,
-	description varchar(255) NULL,
+	description varchar(500) NULL,
 	model varchar(255) NOT NULL,
 	"type" varchar(255) NOT NULL,
 	bike_id uuid NULL,
@@ -24,6 +24,9 @@ CREATE TABLE IF NOT EXISTS public.users (
 );
 
 CREATE INDEX idx_item_type ON public.items("type");
+CREATE INDEX idx_bike_manufacturer ON public.bikes("manufacturer");
+CREATE INDEX idx_bike_name ON public.items("name");
+
 
 INSERT INTO public.users (username,"password") VALUES
 	 ('user2','$2a$10$UnLWzcYjYaHFAfr4qtlPieOZUS32e/w4X0YD7dJOllY3bElDD2v3G'),
