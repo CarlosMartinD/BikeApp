489914ffa5685b258cf565074b8bd0bd6f2eac5b
Your Name <youremail@yourdomain.com>
2024-03-17 20:21:38 +0100

    Improve rest logging

--- bike-control/bike-control-infrastructure/bike-control-rest/src/main/java/com/bkool/rest/ErrorHandler.java
+++ bike-control/bike-control-infrastructure/bike-control-rest/src/main/java/com/bkool/rest/ErrorHandler.java
@@ -1,21 +1,25 @@
-package com.bkool.security;
+package com.bkool.rest;
 
 import exception.AuthenticationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Void> handleMyException(RuntimeException ex) {
+        log.error("ERROR", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Void> handleMyException(AuthenticationException ex) {
+        log.error("ERROR", ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 }
